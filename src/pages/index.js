a150433910c0f341d513a91cdd09be79a3b5c4bc
Kevin Crawford <kcdouglas92@gmail.com>
2019-09-24 19:53:17 -0700

    added new field types to cms config

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <SEO title="Home" keywords={[`Podcast`, `waterpolo`, `Crosspass`]} />
     <h1>Home Page</h1>
     <StaticQuery
       query={indexQuery}
--- src/pages/podcasts/001-first-post.md
+++
@@ -1,8 +0,0 @@
----
-title: "First Post - Introduction"
-date: 2019-09-24 07:00:00
-author: "Kevin Crawford"
-path: "/first-podcast"
----
-
-Welcome to the Crosspass Podcast, hope you enjoy
