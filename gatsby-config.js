b793d7360976e0ac7720b821f00a6212b033f635
Kevin Crawford <kcdouglas92@gmail.com>
2019-09-24 18:00:05 -0700

    modified config for cms, changed source folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,14 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
+      // keep as first gatsby-source-filesystem plugin for gatsby image support
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/static/img`,
+        name: `uploads`,
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
@@ -38,8 +46,6 @@ module.exports = {
         icon: `src/images/cpp-logo.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`,
   ],
 }
